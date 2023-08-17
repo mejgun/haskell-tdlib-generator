1a12a4f28879087dacf3a570b7151cd9351a4802
mejgun <mejgun@gmail.com>
2023-08-17 09:46:26 +0300

    types separate

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,7 +8,7 @@ where
 import Data.Text qualified as T
 import Data.Text.IO qualified as TI
 import Parser (allParser)
-import Post (check)
+import Post ()
 import Pre qualified
 import System.Environment (getArgs)
 import Text.Megaparsec (parse)
@@ -21,7 +21,7 @@ someFunc = do
       pars = parse allParser fname . T.unpack
       func = \case
         Left e -> error $ show e
-        Right res -> check res
+        Right res -> mapM_ print res
   -- TI.putStrLn dat
   -- TI.putStrLn fun
   let (x, _) = (pars dat, pars fun)
--- test/Spec.hs
+++ test/Spec.hs
@@ -10,79 +10,85 @@ main = do
       it "from file" $ do
         M.parse allParser "test file" f
           `shouldBe` Right
-            [ Class "AuthenticationCodeType" "Provides information about the method by which an authentication code is delivered to the user",
-              Method
-                { name = "authenticationCodeTypeTelegramMessage",
-                  comment = "An authentication code is delivered via a private Telegram message, which can be viewed from another active session",
-                  args =
-                    [ Arg
-                        { name = "length",
-                          realname = "length",
-                          value = TInt32,
-                          comment = Just "Left time before the email",
-                          null = False
-                        }
-                    ],
-                  result = "AuthenticationCodeType"
-                },
-              Method
-                { name = "authenticationCodeTypeSms",
-                  comment = "An authentication code is delivered via an SMS message to the specified phone number; applications may not receive this type of code",
-                  args =
-                    [ Arg
-                        { name = "reset_in",
-                          realname = "reset_in",
-                          value = TInt32,
-                          comment = Nothing,
-                          null = False
-                        }
-                    ],
-                  result = "AuthenticationCodeType"
-                },
-              Class
-                { name = "EmailAddressAuthentication",
-                  comment = "Contains authentication data for a email address"
-                },
-              Method
-                { name = "authenticationCodeTypeCall",
-                  comment = "An authentication code is delivered via a phone call to the specified phone number",
-                  args =
-                    [ Arg
-                        { name = "length",
-                          realname = "length",
-                          value = TInt32,
-                          comment = Just "Length of the code",
-                          null = False
-                        }
-                    ],
-                  result = "AuthenticationCodeType"
-                },
-              Method
-                { name = "stickerFullTypeRegular",
-                  comment = "The sticker is a regular sticker",
-                  args =
-                    [ Arg
-                        { name = "premium_animation",
-                          realname = "premium_animation",
-                          value = TModule "file",
-                          comment = Just "Premium animation of the sticker; may be null. If present, only Telegram Premium users can use the sticker",
-                          null = True
-                        }
-                    ],
-                  result = "StickerFullType"
-                },
-              Method
-                { name = "closedVectorPath",
-                  comment = "Represents a closed vector path.",
-                  args =
-                    [ Arg
-                        { name = "commands",
-                          realname = "commands",
-                          value = TVector (TModule "VectorPathCommand"),
-                          comment = Nothing,
-                          null = False
-                        }
-                    ],
-                  result = "ClosedVectorPath"
-                }
+            [ C $ Class "AuthenticationCodeType" "Provides information about the method by which an authentication code is delivered to the user",
+              M $
+                Method
+                  { name = "authenticationCodeTypeTelegramMessage",
+                    comment = "An authentication code is delivered via a private Telegram message, which can be viewed from another active session",
+                    args =
+                      [ Arg
+                          { name = "length",
+                            realname = "length",
+                            value = TInt32,
+                            comment = Just "Left time before the email",
+                            null = False
+                          }
+                      ],
+                    result = "AuthenticationCodeType"
+                  },
+              M $
+                Method
+                  { name = "authenticationCodeTypeSms",
+                    comment = "An authentication code is delivered via an SMS message to the specified phone number; applications may not receive this type of code",
+                    args =
+                      [ Arg
+                          { name = "reset_in",
+                            realname = "reset_in",
+                            value = TInt32,
+                            comment = Nothing,
+                            null = False
+                          }
+                      ],
+                    result = "AuthenticationCodeType"
+                  },
+              C $
+                Class
+                  { name = "EmailAddressAuthentication",
+                    comment = "Contains authentication data for a email address"
+                  },
+              M $
+                Method
+                  { name = "authenticationCodeTypeCall",
+                    comment = "An authentication code is delivered via a phone call to the specified phone number",
+                    args =
+                      [ Arg
+                          { name = "length",
+                            realname = "length",
+                            value = TInt32,
+                            comment = Just "Length of the code",
+                            null = False
+                          }
+                      ],
+                    result = "AuthenticationCodeType"
+                  },
+              M $
+                Method
+                  { name = "stickerFullTypeRegular",
+                    comment = "The sticker is a regular sticker",
+                    args =
+                      [ Arg
+                          { name = "premium_animation",
+                            realname = "premium_animation",
+                            value = TModule "file",
+                            comment = Just "Premium animation of the sticker; may be null. If present, only Telegram Premium users can use the sticker",
+                            null = True
+                          }
+                      ],
+                    result = "StickerFullType"
+                  },
+              M $
+                Method
+                  { name = "closedVectorPath",
+                    comment = "Represents a closed vector path.",
+                    args =
+                      [ Arg
+                          { name = "commands",
+                            realname = "commands",
+                            value = TVector (TModule "VectorPathCommand"),
+                            comment = Nothing,
+                            null = False
+                          }
+                      ],
+                    result = "ClosedVectorPath"
+                  }
             ]
